ff703f4f988dc5e0f444f974f03aa23fb5e62247
“reneetsang” <673464811@qq.com>
2022-05-07 16:37:40 +0800

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: '蚝仔米',
+  // tagline: 'Dinosaurs are cool',
+  url: 'https://haozaimi.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: '蚝仔米',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
